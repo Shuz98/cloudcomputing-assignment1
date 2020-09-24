ed442098bcdb76cfaba063230d7471bbaeb4a00b
Shuz98 <zhitao.shu@vanderbilt.edu>
2020-09-24 15:51:25 -0500

    Update producer.py

--- producer.py
+++ producer.py
@@ -23,7 +23,7 @@ from kafka import KafkaProducer  # producer of events
 
 # acquire the producer
 # (you will need to change this to your bootstrap server's IP addr)
-producer = KafkaProducer (bootstrap_servers="129.114.27.100:9092", 
+producer = KafkaProducer (bootstrap_servers="129.114.25.82:9092", 
                                           acks=1)  # wait for leader to write to log
 
 # say we send the contents 100 times after a sleep of 1 sec in between
